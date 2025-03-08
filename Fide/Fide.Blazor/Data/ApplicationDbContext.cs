e1933626f704c97ad4fc6dcae3ca16fafa34ae1f
Danil <dan2002.dan2002@mail.ru>
2025-03-08 20:23:13 +0500

    Попытки подключится к БД

--- Fide/Fide.Blazor/Data/ApplicationDbContext.cs
+++ Fide/Fide.Blazor/Data/ApplicationDbContext.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Fide.Blazor.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        if (Database.GetService<IDatabaseCreator>() is RelationalDatabaseCreator dbCreater)
+        {
+            if (!dbCreater.CanConnect())
+            {
+                dbCreater.Create();
+            }
+            if (!dbCreater.HasTables())
+            {
+                dbCreater.CreateTables();
+            }
+        }
+    }
 }
